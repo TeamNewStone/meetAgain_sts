a15c2a7238becd6411451309e4621676e3ba7787
user1 <user1@DESKTOP-QEG4PSG>
2020-08-04 16:31:15 +0900

    2020-08-04_16:31 관리자페이지 모임관리 등 commit

--- meetAgain_sts/src/main/java/com/kh/meetAgain/admin/model/dao/AdminDAO.java
+++ meetAgain_sts/src/main/java/com/kh/meetAgain/admin/model/dao/AdminDAO.java
@@ -51,5 +51,13 @@ public interface AdminDAO {
 	Member selectOneMember(String userId);
 
 	List<Map<String, String>> selectMemberOrderList(int cPage, int numPerPage, Map<String, String> map);
+
+	List<Map<String, String>> selectGroupList1(int cPage, int numPerPage);
+
+	int selectGroupTotalContents1();
+
+	List<Map<String, String>> selectGroupList2(int cPage, int numPerPage);
+
+	int selectGroupTotalContents2();
 	
 }
