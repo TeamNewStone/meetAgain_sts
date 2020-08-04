5fcb64cd538bf02bf912d7c74d39aedd484fb7f2
kh_jsk <user1@DESKTOP-2NG221G>
2020-08-04 16:02:32 +0900

    2020-08-04_(16_01_06)

--- meetAgain_sts/src/main/java/com/kh/meetAgain/sgroup/model/vo/Calendar.java
+++ meetAgain_sts/src/main/java/com/kh/meetAgain/sgroup/model/vo/Calendar.java
@@ -1,6 +1,7 @@
 package com.kh.meetAgain.sgroup.model.vo;
 
 import java.sql.Timestamp;
+import java.io.Serializable;
 import java.sql.Date;
 
 import lombok.AllArgsConstructor;
@@ -10,14 +11,14 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor // 기본 생성자용
 @AllArgsConstructor // 전체 매개변수 받는 생성자 자동 생성 어노테이션
-public class Calendar {
+public class Calendar implements Serializable {
 	
+	private static final long serialVersionUID = 999999L;
 	private String gid;
 	private Date gdate;
 	private Date gdateEnd;
 	private String ginfo;
-	// private String gtimeStr; // 시간
 	private Timestamp gtime;	
 	private String isctn;
-
+	
 }
