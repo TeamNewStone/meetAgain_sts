5652af2ef58636e4810a4357e313a69276423c88
wooseok <thenameisdifficult@gmail.com>
2020-08-09 00:54:27 +0900

    200809 0052 삭제 후 목록으로 돌아가기, 뒤로가기 수정폼 썸머노트

--- meetAgain_sts/src/main/java/com/kh/meetAgain/sgroup/controller/SgroupController.java
+++ meetAgain_sts/src/main/java/com/kh/meetAgain/sgroup/controller/SgroupController.java
@@ -230,7 +230,7 @@ public class SgroupController {
 		 if(result > 0) msg = "모임 탈퇴 성공!"; 
 		 else msg = "모임 탈퇴 실패!";
 
-		  model.addAttribute("loc", loc); 
+		  model.addAttribute("loc", loc);
 		  model.addAttribute("msg", msg);
 
 		return "common/msg";
@@ -280,15 +280,19 @@ public class SgroupController {
 	}
 	
 	@RequestMapping("/sgroup/groupBoardDetail.do")
-	public String groupBoardDetail(@RequestParam int gbId, Model model) {
-
+	public String groupBoardDetail(@RequestParam int gbId, @RequestParam("gid") String gid, Model model) {
+		
 		Gboard gb = sgroupService.SelectOnegBoard(gbId);
+		Sgroup s = sgroupService.selectOneSgroup(gid);
 		List<GB_comment> list = sgroupService.selectCommentList(gbId);
 
 		int gbRate = sgroupService.updateReadCount(gbId);
+		model.addAttribute("sgroup", s);
 		model.addAttribute("gbRate", gbRate);
 		model.addAttribute("list", list);
 		model.addAttribute("Gboard", gb);
+		model.addAttribute("gid", gid);
+		System.out.println("sgroup : "+s);
 		System.out.println("Detail controller list : " + list);
 
 		return "/sgroup/groupBoardDetail";
@@ -309,7 +313,6 @@ public class SgroupController {
 		Gboard.setGId(gid);
 		Gboard.setUserId(userid);
 		int result = sgroupService.insertgBoard(Gboard);
-		
 		String loc = "/sgroup/groupBoard.do";
 		String msg = "";
 		if (result > 0) {
@@ -329,33 +332,35 @@ public class SgroupController {
 
 
 	@RequestMapping("sgroup/groupBoardUpdate.do")
-	public String noticeUpdate(int gbId, Model model) {
+	public String gBoardUpdate(@RequestParam int gbId, @RequestParam String gid, Model model) {
 		model.addAttribute("Gboard", sgroupService.SelectOnegBoard(gbId));
+		model.addAttribute("gid", gid);
+		System.out.println("model에 gbId가 있는지 확인 : " + model);
+
 		return "/sgroup/groupBoardUpdateForm";
 	}
 
 	@RequestMapping("/sgroup/gbUpdate.do")
-	public String gbUpdate(Gboard Gboard, Model model) {
+	public String gbUpdate(@RequestParam("gid") String gid,@RequestParam("gbId") int gbId, Gboard Gboard, Model model) {
 		int result = sgroupService.updategBoard(Gboard);
-
-		String loc = "sgroup/groupBoard.do";
+		System.out.println("result ? : " + result);
+		String loc = "sgroup/groupBoardDetail.do";
 		String msg = "";
 
 		if (result > 0) {
 			msg = "게시글 수정이 완료되었습니다";
-			loc = "/sgroup/groupBoardDetail.do?gbId=" + Gboard.getGbId();
+			loc = "/sgroup/groupBoardDetail.do?gbId="+gbId+"&gid="+gid; 
 		} else {
 			msg = "게시글 수정 실패. 다시 시도해주세요";
 		}
 
 		model.addAttribute("loc", loc).addAttribute("msg", msg);
-		System.out.println("updateController : " + Gboard);
 
 		return "common/msg";
 	}
 
 	@RequestMapping("/sgroup/groupBoardDelete.do")
-	public String groupBoardDelete(@RequestParam int gbId, HttpSession session, Model model) {
+	public String groupBoardDelete(@RequestParam int gbId, @RequestParam String gid, Model model) {
 		int result = sgroupService.deletegBoard(gbId);
 
 		String loc = "sgroup/groupBoard.do";
@@ -363,28 +368,18 @@ public class SgroupController {
 
 		if (result > 0) {
 			msg = "게시글 등록 성공!";
-			loc = "/sgroup/groupBoardDetail.do?gbId="+gbId;
+			loc = "/sgroup/groupBoard.do?&gid="+gid;
 		} else {
 			msg = "게시글 삭제 실패!";
 		}
 
+		model.addAttribute("gid", gid);
 		model.addAttribute("loc", loc).addAttribute("msg", msg);
-		System.out.println("deleteController session : " + session);
+
 		System.out.println("deleteController model : " + model);
 		return "common/msg";
 	}
 
-	@RequestMapping("/sgroup/selectGboardComment.do")
-	public String listComment(Model model) {
-
-		/*
-		 * List<GB_comment> list = sgroupService.selectCommentList();
-		 * 
-		 * model.addAttribute("list", list);
-		 */
-		return "/sgroup/groupBoard.do";
-
-	}
 
 	@RequestMapping("sgroup/gotoGroup.do")
 	public String gotoGroup(@RequestParam String gid, Model model) {
@@ -399,7 +394,7 @@ public class SgroupController {
 	}
 	
 	@RequestMapping("/sgroup/insertComment.do")
-	public String insertComment(@RequestParam("gbId") int gbId, GB_comment GB_comment, Model model) {
+	public String insertComment(@RequestParam("gbId") int gbId,@RequestParam("gid") String gid , GB_comment GB_comment, Model model) {
 
 		GB_comment.setGbId(gbId);
 		int result = sgroupService.insertComment(GB_comment);
@@ -409,7 +404,7 @@ public class SgroupController {
 		String msg = "";
 		if (result > 0) {
 			msg = "댓글 등록 성공!";
-			loc = "/sgroup/groupBoardDetail.do?gbId=" + gbId;
+			loc = "/sgroup/groupBoardDetail.do?gbId=" + gbId + "&gid="+ gid; 
 
 		} else {
 			msg = "댓글 등록 실패!";
@@ -421,22 +416,24 @@ public class SgroupController {
 
 	}
 	@RequestMapping("/sgroup/commentUpdate.do")
-	public String commentUpdate(@RequestParam("cId") int cId, GB_comment GB_comment, Model model) {
-		
+	public String commentUpdate(@RequestParam("gbId") int gbId, @RequestParam("cId") int cId, @RequestParam("gid") String gid, GB_comment GB_comment, Model model) {
+
 		GB_comment.setCId(cId);
+		
 		int result = sgroupService.commentUpdate(GB_comment);
 
 		String loc = "/sgroup/insertComment.do";
 		String msg = "";
 		if (result > 0) {
-			msg = "댓글 등록 성공!";
-			loc = "/sgroup/groupBoardDetail.do?gbId=" + GB_comment.getGbId();
+			msg = "댓글 수정 성공!";
+			loc = "/sgroup/groupBoardDetail.do?gbId=" + GB_comment.getGbId() + "&gid=" + gid;
 
 		} else {
-			msg = "댓글 등록 실패!";
+			msg = "댓글 수정 실패!";
 		}
 
 		model.addAttribute("GB_comment", sgroupService.commentUpdate(GB_comment));
+		model.addAttribute("gid", gid);
 		model.addAttribute("loc", loc).addAttribute("msg", msg);
 		System.out.println("controller GB" + GB_comment);
 		return "common/msg";
@@ -446,7 +443,7 @@ public class SgroupController {
 
 
 	@RequestMapping("/sgroup/commentDelete.do")
-	public String commentDelete(@RequestParam("cId") int cId, Model model, @RequestParam("gbId") int gbId) {
+	public String commentDelete(@RequestParam("cId") int cId, Model model, @RequestParam("gbId") int gbId,@RequestParam("gid") String gid) {
 			
 		try { 
 			int result = sgroupService.commentDelete(cId);
@@ -455,13 +452,13 @@ public class SgroupController {
 			String msg = "";
 		//	int gid = gB_comment.getGbId();
 			if(result > 0) {
-				loc ="/sgroup/groupBoardDetail.do?gbId="+ gbId;
-				msg = "댓글 작성";
+				loc ="/sgroup/groupBoardDetail.do?gbId="+ gbId+"&gid="+gid;
+				msg = "댓글 삭제 성공!";
 			}
 			else msg = "댓글 삭제 실패!";
 			
 			model.addAttribute("loc", loc);
-			model.addAttribute("msg", msg);		
+			model.addAttribute("msg", msg);
 			System.out.println("gB_comment.getGbId() : " +gbId);
 		} catch(Exception e) {
 			
--- meetAgain_sts/src/main/webapp/WEB-INF/views/sgroup/groupBoardDetail.jsp
+++ meetAgain_sts/src/main/webapp/WEB-INF/views/sgroup/groupBoardDetail.jsp
@@ -6,10 +6,9 @@
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
 <%@ page
 	import="java.util.*, com.kh.meetAgain.sgroup.model.vo.*, com.kh.meetAgain.member.model.vo.*"%>
-
-<c:import url="/WEB-INF/views/common/header.jsp" /><div class="container" style="taxt-align: center;">
+<c:import url="/WEB-INF/views/common/header.jsp" />
+<div class="container" style="taxt-align: center;">
 	<c:import url="/WEB-INF/views/common/groupHeader.jsp" />
-
 	<div class="row">
 		<div class="col-12">
 			<div class="content" align="center">
@@ -28,9 +27,9 @@
 				<div class="col-3"></div>
 				<div class="col-3"></div>
 				<div class="col-3">
-				<a class="btn_1" onclick="goDelete()">삭제하기</a>
-				<a class="btn_1" onclick="goUpdate()">수정하기</a>
-					<%-- <a class="btn_1 checkout_btn_1" href="<%=request.getContextPath()%>/sgroup/groupBoard.do?gId=${gb.getGId()}">메뉴로 돌아가기</a> --%>
+					<a class="btn_1" onclick="goDelete()">삭제하기</a> <a class="btn_1"
+						onclick="goUpdate()">수정하기</a> <a class="btn_1 checkout_btn_1"
+						onclick="goBack()">뒤로 가기</a>
 
 				</div>
 				<div class="replyArea">
@@ -42,7 +41,8 @@
 								type="hidden" name="gbId" value="${Gboard.gbId}" /> <input
 								type="hidden" name="cRef" value="1" /> <input type="hidden"
 								name="cLevel" value="1" /> <input type="hidden" name="cRec"
-								value="0" /> <input type="hidden" name="cDel" value="N" />
+								value="0" /> <input type="hidden" name="cDel" value="N" /> <input
+								type="hidden" name="gid" value="${gid}" />
 							<table>
 								<tr>
 									<td><textArea rows="3" cols="80" id="cContent"
@@ -66,14 +66,13 @@
 									class="replyList${gbc.getCLevel()}">
 									<tr>
 										<td rowspan="2"></td>
-										<td>${gbc.getCDate()}${gbc.getNickName()}</td>
+										<td>${gbc.getCDate()}${member.getNickName()}</td>
 										<td align="center"><c:if
 												test="${ member.userId eq gbc.getUserId()}">
 												<input type="hidden" name="cId" value="${gbc.getCId()}" />
 
 												<button type="button" class="updateBtn"
 													onclick="updateReply(this);">수정하기</button>
-
 												<button type="button" class="updateConfirm"
 													onclick="updateConfirm(this);" style="display: none;">수정완료</button> &nbsp;&nbsp;
 												<button type="button" class="deleteBtn"
@@ -117,22 +116,19 @@
 		</div>
 	</div>
 	<br>
-
 </div>
 <script>
-
-// 	function goGboard() {
-// 		location.href = "${ pageContext.request.contextPath}/sgroup/groupBoard.do"
-// 	}
+	// 	function goGboard() {
+	// 		location.href = "${ pageContext.request.contextPath}/sgroup/groupBoard.do"
+	// 	}
 	function goUpdate() {
-		var gbId = "${Gboard.gbId}"
-		;
-		location.href = "${ pageContext.request.contextPath}/sgroup/groupBoardUpdate.do?gbId="+gbId;
+		location.href = "${ pageContext.request.contextPath}/sgroup/groupBoardUpdate.do?gbId="+ ${Gboard.gbId} + "&gid=" + ${gid};
 	}
 	function goDelete() {
-		var gbId = "${Gboard.gbId}"
-		;
-		location.href = "${ pageContext.request.contextPath}/sgroup/groupBoardDelete.do?gbId="+gbId;
+		location.href = "${ pageContext.request.contextPath}/sgroup/groupBoardDelete.do?gbId="+ ${Gboard.gbId} + "&gid=" + ${gid};
+	}
+	function goBack() {
+		location.href = "${ pageContext.request.contextPath}/sgroup/groupBoard.do?gid="+ ${gid};
 	}
 
 	function updateReply(obj) {
@@ -156,9 +152,8 @@
 		// 게시글 번호 가져오기
 		var gbId = "${Gboard.gbId}";
 
-		location.href = "${ pageContext.request.contextPath}/sgroup/commentUpdate.do?"
-				+ "cId=" + cId + "&gbId=" + gbId + "&cContent=" + content;
-	}
+		location.href = "${ pageContext.request.contextPath}/sgroup/commentUpdate.do?gid="+${gid}+"&cId=" + cId + "&gbId=" + gbId + "&cContent=" + content;
+		}
 
 	function deleteReply(obj) {
 		// 댓글의 번호 가져오기
@@ -167,9 +162,9 @@
 		// 게시글 번호 가져오기
 		var gbId = "${Gboard.gbId}";
 
-		location.href = "${ pageContext.request.contextPath}/sgroup/commentDelete.do"
-				+ "?cId=" + cId+"&gbId="+gbId;
-	}
+		location.href = "${ pageContext.request.contextPath}/sgroup/commentDelete.do?cId="
+				+ cId + "&gbId=" + gbId + "&gid=" + ${gid};
+		}
 
 	function reComment(obj) {
 		// 추가 완료 버튼을 화면 보이게 하기
@@ -214,6 +209,7 @@
 				+ '&gbId=' + gbId + '&cRef=' + cRef + '&cLevel=' + cLevel;
 	}
 </script>
+
 <script>
 	console.log("${list}");
 </script>
